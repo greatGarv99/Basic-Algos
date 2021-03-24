f83ef845cc86e07482db2d35e0611c907f512f22
Garv <garv.programming@gmail.com>
2021-03-24 22:43:14 +0530

    Binary-search via recursion

--- binarySearch.py
+++ binarySearch.py
@@ -13,6 +13,16 @@ def binary_search(arr, item):
   return "Item not found."
 
 
+def binarySearchRecurse(arr, item):
+  # Base case
+  if len(arr) == 1: return arr[0] if arr[0] == item else 'Item not found.'
+
+  # Recursive case
+  if arr[len(arr)//2] == item: return f'{item} found at index {len(arr)//2}'    
+  elif arr[len(arr)//2] > item: return binarySearchRecurse(arr[:len(arr)//2-1], item)
+  elif arr[len(arr)//2] < item: return binarySearchRecurse(arr[len(arr)//2+1:], item)
+    
+
 l = [int(i) for i in input("Enter a list: ").split()]
 item = int(input("Enter the item to search: "))
-print(binary_search(l, item))
+print(binarySearchRecurse(l, item))
